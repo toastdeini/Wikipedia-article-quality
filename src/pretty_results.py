f09df7a547244363666b305fec494c8cfd5c5865
Luke Dowker <lhdowker@gmail.com>
2022-05-18 17:15:53 -0400

    Fixing syntax in function

--- src/pretty_results.py
+++ src/pretty_results.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import numpy as np
 
-def pretty_cv(results):
+def pretty_cv(result):
     '''
     Generic function to print 'pretty' results from the results of `cross_validate`.
     
-    :param results: Output of the sklearn `cross_validate` method.
+    :param cv_metric: Output of the sklearn `cross_validate` method.
     :return: Printout of accuracy and F1 scores on both training and validation sets.
     '''
     print('CV Results')
     print('='*32)
     print('Accuracy')
     print('-'*32)
-    print(f"Training accuracy: {cv_metric['train_accuracy'].mean():.3f}")
-    print(f"Test accuracy:     {cv_metric['test_accuracy'].mean():.3f}")
+    print(f"Training accuracy: {result['train_accuracy'].mean():.3f}")
+    print(f"Test accuracy:     {result['test_accuracy'].mean():.3f}")
     print('F-1 Score')
     print('-'*32)
-    print(f"Training F1 score: {cv_metric['train_f1_macro'].mean():.3f}")
-    print(f"Test F1 score:     {cv_metric['test_f1_macro'].mean():.3f}")
+    print(f"Training F1 score: {result['train_f1_macro'].mean():.3f}")
+    print(f"Test F1 score:     {result['test_f1_macro'].mean():.3f}")
