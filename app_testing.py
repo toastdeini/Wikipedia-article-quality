2e0852cacc4c59811dda98778a446f9f9bb36282
Luke Dowker <lhdowker@gmail.com>
2022-05-26 17:10:04 -0400

    WIP

--- app_testing.py
+++ app_testing.py
@@ -1,10 +1,10 @@
 # Imports
 import streamlit as st
 import pickle
-# import parse_it
+from src.parse_it import *
 
 f = open('models/xgb_model.sav', 'rb')
-final_model = pickle.load(f)
+model = pickle.load(f)
 
 decoder = {
     0 : 'Good article!',
@@ -12,3 +12,7 @@ decoder = {
 }
 
 st.title("Is this Wikipedia article up to snuff?")
+
+# article = st.text_area("Paste the text of the article you want to analyze here!", max_chars=100000)
+
+# pred = model.predict(parse_doc(article))
